0f9210773bf6d9171bb140350fea2a621e847182
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2025-12-09 21:10:36 +0100

    rename server to dev-server

--- dev-server.js
+++ dev-server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 4711;
+const PORT = process.env.PORT || 4711;
 const clients = [];
 
 const watchFiles = (dir) => {
