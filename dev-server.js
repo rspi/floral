dbb05ebd3b6b3bfe6c32588288509d84cc993035
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2026-01-07 16:30:27 +0100

    refactor dev server

--- dev-server.js
+++ dev-server.js
@@ -23,8 +23,38 @@ const sendReload = () => {
   });
 };
 
-const server = http.createServer((req, res) => {
-  const filePath = req.url === "/" ? "index.html" : `.${req.url}`;
+const getPolyfillScripts = (req) => {
+  const userAgent = req.headers["user-agent"];
+  if (userAgent && userAgent.includes("Firefox")) {
+    return `<script async src="https://ga.jspm.io/npm:es-module-shims@1.10.0/dist/es-module-shims.js"></script>
+           <script type="esms-options">{ "polyfillEnable": ["css-modules"] }</script>`;
+  }
+  return "";
+};
+
+const handleEvents = (req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+  clients.push(res);
+  req.on("close", () => {
+    clients.splice(clients.indexOf(res), 1);
+  });
+};
+
+const handleBlank = (req, res) => {
+  const polyfillScripts = getPolyfillScripts(req);
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    ${polyfillScripts}
+    <script type="module" src="./components/index.js"></script>
+    <link rel="stylesheet" href="variables.css" />`);
+};
+
+const serveFile = (req, res, filePath) => {
   const extname = path.extname(filePath);
   let contentType = "text/html";
 
@@ -37,29 +67,6 @@ const server = http.createServer((req, res) => {
       break;
   }
 
-  if (req.url === "/events") {
-    res.writeHead(200, {
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      Connection: "keep-alive",
-    });
-    clients.push(res);
-    req.on("close", () => {
-      clients.splice(clients.indexOf(res), 1);
-    });
-    return;
-  }
-
-  if (req.url === "/blank") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`
-    <script async src="https://ga.jspm.io/npm:es-module-shims@1.10.0/dist/es-module-shims.js"></script>
-    <script type="esms-options">{ "polyfillEnable": ["css-modules"] }</script>
-    <script type="module" src="./components/index.js"></script>
-    <link rel="stylesheet" href="variables.css" />`);
-    return;
-  }
-
   fs.readFile(filePath, (err, content) => {
     if (err) {
       res.writeHead(404);
@@ -69,14 +76,12 @@ const server = http.createServer((req, res) => {
 
     if (filePath === "index.html") {
       let contentString = content.toString();
-      const userAgent = req.headers["user-agent"];
+      const polyfillScripts = getPolyfillScripts(req);
 
-      if (userAgent && userAgent.includes("Firefox")) {
+      if (polyfillScripts) {
         contentString = contentString.replace(
           "</head>",
-          `<script async src="https://ga.jspm.io/npm:es-module-shims@1.10.0/dist/es-module-shims.js"></script>
-           <script type="esms-options">{ "polyfillEnable": ["css-modules"] }</script>
-           </head>`,
+          `${polyfillScripts}</head>`,
         );
       }
 
@@ -99,7 +104,26 @@ const server = http.createServer((req, res) => {
       res.end(content);
     }
   });
-});
+};
+
+const requestHandler = (req, res) => {
+  switch (req.url) {
+    case "/":
+      serveFile(req, res, "index.html");
+      break;
+    case "/events":
+      handleEvents(req, res);
+      break;
+    case "/blank":
+      handleBlank(req, res);
+      break;
+    default:
+      serveFile(req, res, `.${req.url}`);
+      break;
+  }
+};
+
+const server = http.createServer(requestHandler);
 
 watchFiles("./");
 
