18348ee29ef2c0bbc7b251ef24815a52a50d0ec8
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2026-03-04 12:35:12 +0100

    Reflect Attributes to properties for all web components

--- utils.js
+++ utils.js
@@ -28,16 +28,52 @@ export class CustomElement extends HTMLElement {
     this.shadowRoot.appendChild(
       this.constructor.templateNode.content.cloneNode(true),
     );
+
+    this.#reflectAttributes();
+  }
+
+  #reflectAttributes() {
+    if (this.constructor.meta?.attributes) {
+      for (const attrName of Object.keys(this.constructor.meta.attributes)) {
+        if (!Object.hasOwn(this, attrName)) {
+          Object.defineProperty(this, attrName, {
+            get() {
+              if (this.constructor.meta.attributes[attrName].includes("")) {
+                return this.hasAttribute(attrName);
+              }
+              return this.getAttribute(attrName);
+            },
+            set(value) {
+              if (this.constructor.meta.attributes[attrName].includes("")) {
+                if (value) {
+                  this.setAttribute(attrName, "");
+                } else {
+                  this.removeAttribute(attrName);
+                }
+              } else {
+                if (value === null || value === undefined) {
+                  this.removeAttribute(attrName);
+                } else {
+                  this.setAttribute(attrName, value);
+                }
+              }
+            },
+            configurable: true,
+            enumerable: true,
+          });
+        }
+      }
+    }
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    this.validateAttributes(newValue, name);
+    this.#validateAttributes(newValue, name);
     if (this.attributesChanged) {
       this.attributesChanged(name, oldValue, newValue);
     }
   }
 
-  validateAttributes(value, name) {
+  #validateAttributes(value, name) {
     const { attributes } = this.constructor.meta;
     if (value !== null && !attributes[name].includes(value)) {
       throw new Error(`${this.tagName.toLowerCase()} got an unexpected value for argument ${JSON.stringify(name)}:
